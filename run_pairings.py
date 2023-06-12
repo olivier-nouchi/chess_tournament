57c715f97ed8a7474c0e8ab7fdf2f0cec1feb52d
oliviernouchi <olivier.nouchi@gmail.com>
2023-06-13 00:41:28 +0300

    working version pairings

--- run_pairings.py
+++ run_pairings.py
@@ -11,10 +11,10 @@ from pairings import create_pairings
 from utils import PairingMethod, display_players_ranking, display_players_stats
 
 # ENTER WHICH ROUND IT IS
-ROUND_NUMBER = 4
+ROUND_NUMBER = 1
 
 if __name__ == "__main__":
-    print(players_from_spreadsheet)
+    # print(players_from_spreadsheet)
     create_pairings(players=players_from_spreadsheet, round_number=ROUND_NUMBER, method=PairingMethod.points)
 
     display_players_ranking(players=players_from_spreadsheet)
